36eae3bc58235429563e1958ee0bc407bb67c964
himantyu-yuma <lipton.happy.milktea@gmail.com>
2021-03-04 00:36:19 +0900

    postgreを動かしたい

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+
+const db = require("./database");
 
 const app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+app.get("/db/", (req, res) => {
+  res.send("hello");
+  db.pool.connect((err, client) => {
+    if (err) {
+      console.log(err);
+    } else {
+      client.query("SELECT name FROM rank", (err, result) => {
+        console.log(result.rows);
+      });
+    }
+  });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('server started '+port);
+console.log("server started " + port);
