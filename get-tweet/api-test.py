e43282b7000613da6faabfa7f376b9e9fba98f17
himantyu-yuma <lipton.happy.milktea@gmail.com>
2021-03-08 22:23:10 +0900

    TwitterAPIのキーとかやってなかった

--- get-tweet/api-test.py
+++ get-tweet/api-test.py
@@ -1,21 +1,75 @@
-import config
+"""
+API使うやつ
+"""
+# import config
 from requests_oauthlib import OAuth1Session
 import json
+from datetime import *
 
+import os
+import psycopg2
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-api_key = config.API_KEY
-api_secret_key = config.API_SECRET_KEY
-access_token = config.ACCESS_TOKEN
-access_token_secret = config.ACCESS_TOKEN_SECRET
+# 収集対象のルーム
+room = 'tracka'
+
+# DB関連の準備
+DATABASE_URL = os.getenv('DATABASE_URL')
+engine = create_engine(DATABASE_URL)
+
+Base = declarative_base()
+
+# スキーム定義
+class Tweet(Base):
+    __tablename__ = room
+    id = Column(Integer, primary_key=True)
+    tweet_str_id = Column(TEXT)
+    created_at = Column(DATETIME)
+
+SessionClass = sessionmaker(bind=engine)
+session = SessionClass()
+
+# テーブル作成
+Base.metadata.create_all(engine)
+
+# Twitterからデータ取得
+api_key = os.getenv('TW_API_KEY')
+api_secret_key = os.getenv('TW_API_SECRET_KEY')
+access_token = os.getenv('TW_ACCESS_TOKEN')
+access_token_secret = os.getenv('TW_ACCESS_TOKEN_SECRET')
 
 twitter = OAuth1Session(api_key, api_secret_key, access_token, access_token_secret)
 
 url = 'https://api.twitter.com/1.1/search/tweets.json'
-params = {'q' : '%23坂道テレビ%20%23櫻坂46%20exclude:retweets', 'count': 100}
+# params = {'q' : '#DeNATechCon since:2021-03-03_06:00:00_JST until:2021-03-03_08:00:00_JST exclude:retweets', 'count': 100}
+params = { 'q': '#DeNATechCon since:2021-03-03_13:00:00_JST until:2021-03-03_14:00:00_JST exclude:retweets', 'count': 100 }
 
 res = twitter.get(url, params = params)
 
-with open('api-out.json', mode='w', encoding='utf-8') as f:
-    tweets = json.loads(res.text)
-    json.dump(tweets, f, ensure_ascii=False)
+tweets = json.loads(res.text)
+
+store_data = [
+        Tweet(tweet_str_id=v['id_str'],
+        # 'tweet_time': datetime.strptime(v['created_at'], '%a %b %d %H:%M:%S %z %Y').astimezone(timezone(timedelta(hours=+9))).strftime('%Y-%m-%d %H:%M:%S')
+        'tweet_time': datetime.strptime(v['created_at'], '%a %b %d %H:%M:%S %z %Y').astimezone(timezone(timedelta(hours=+9)))
+         ) for v in tweets['statuses']]
+
+session.bulk_save_objects(
+    [Test(name=d) for d in data_list], return_defaults=True
+)
+
+session.commit()
+
+# jsonに保存
+# with open('api-out.json', mode='w', encoding='utf-8') as f:
+#     tweets = json.loads(res.text)
+#     store_data = [
+#         {'tweet_id': v['id_str'],
+#         # 'tweet_time': datetime.strptime(v['created_at'], '%a %b %d %H:%M:%S %z %Y').astimezone(timezone(timedelta(hours=+9))).strftime('%Y-%m-%d %H:%M:%S')
+#         'tweet_time': datetime.strptime(v['created_at'], '%a %b %d %H:%M:%S %z %Y').astimezone(timezone(timedelta(hours=+9)))
+#         } for v in tweets['statuses']]
+#     # json.dump(tweets, f, ensure_ascii=False)
+#     json.dump(store_data, f, ensure_ascii=False)
 # print(tweets)
--- get-tweet/get-tweet.py
+++ get-tweet/get-tweet.py
@@ -1,12 +1,12 @@
 import twint
 c = twint.Config()
 c.Search = '#技育祭'
-c.Search = '#RoomC'
+# c.Search = '#RoomC'
 # c.Since = '2020-07-03 15:00:00'
 # c.Until = '2020-07-05 15:00:00'
 c.Since = '2020-07-04 00:00:00'
 c.Until = '2020-07-06 00:00:00'
 c.Store_json = True
-c.Output = 'roomc.json'
+c.Output = 'count_hashtag.json'
 c.Hide_output = True
 twint.run.Search(c)
