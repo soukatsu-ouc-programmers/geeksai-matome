fa515248b4b01ab59e2e5277ae06c6f0be145b75
himantyu-yuma <lipton.happy.milktea@gmail.com>
2021-03-10 23:17:31 +0900

    引数取れるかな？

--- get-tweet/schedule-test.py
+++ get-tweet/schedule-test.py
@@ -4,24 +4,22 @@ from datetime import *
 sched = BlockingScheduler()
 
 # @sched.scheduled_job('date', run_date=datetime(2021, 3, 13, 12, 10, 0))
-# def timed_job():
+# def session1():
 #    print('This job is run every three minutes.')
 # @sched.scheduled_job('cron', hour=14, minute=40, year=2021, month=3, day='11-13')
-# def timed_job():
+# def session2():
 #    print('This job is run every three minutes.')
 # @sched.scheduled_job('cron', hour=15, minute=50, year=2021, month=3, day='11-13')
-# def timed_job():
+# def session3():
 #    print('This job is run every three minutes.')
 # @sched.scheduled_job('cron', hour=17, minute=0, year=2021, month=3, day='11-13')
-# def timed_job():
+# def session4():
 #    print('This job is run every three minutes.')
 # @sched.scheduled_job('cron', hour=18, minute=10, year=2021, month=3, day='11-13')
-# def timed_job():
+# def session5():
 #    print('This job is run every three minutes.')
-@sched.scheduled_job('cron', hour=22, minute=20)
-def timed_job():
-   print('hello1')
-@sched.scheduled_job('cron', hour=22, minute=21)
-def timed_job():
-   print('hello2')
+@sched.scheduled_job('cron', hour=23, minute=18, args=['aaaaa'])
+def getTweet(key):
+   print(key)
+
 sched.start()
